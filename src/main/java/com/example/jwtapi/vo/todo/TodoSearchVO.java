b657a0dc6126af207290bfd4295cce1d8528bb6f
sohot8653 <kmbin92@stam.kr>
2025-05-13 10:26:05 +0900

    [Swagger 수정]

--- src/main/java/com/example/jwtapi/vo/todo/TodoSearchVO.java
+++ src/main/java/com/example/jwtapi/vo/todo/TodoSearchVO.java
@@ -1,9 +1,13 @@
 package com.example.jwtapi.vo.todo;
 
 import lombok.Data;
+import io.swagger.v3.oas.annotations.Parameter;
 
 @Data
 public class TodoSearchVO {
+    @Parameter(description = "검색 키워드 (제목 또는 내용)", required = false)
     private String keyword;
+    
+    @Parameter(description = "완료 상태", required = false)
     private Boolean completed;
 } 
